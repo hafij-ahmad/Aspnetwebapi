71085ce8b5009f00d1e7d09c3169626bdeddefc0
HAFIJ AHMAD <hafijahmad86@yahoo.in>
2025-04-05 05:03:38 +0530

    craeting web api crud operation
    Creating web api web api controller part code

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using WebApiContrib.Formatting.Jsonp;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+
+
+namespace EmployeeServices
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable attribute routing
+            config.MapHttpAttributeRoutes();
+
+            // Set up the JSON formatter for the API
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Add application/json support
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"));
+
+            // Optional: Remove XML Formatter if you don't need it
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Default Web API route
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            //var jsonpformatters = new JsonpMediaTypeFormatter(config.Formatters.JsonFormatter);
+            //config.Formatters.Insert(0, jsonpformatters);
+
+            //config.Formatters.Add(new CustomJsonFormatter());
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"));
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            
+           
+
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAcess;
+
+namespace EmployeeServices.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //WebApiEntities Entity = new WebApiEntities();
+        public IEnumerable<Employe> Get()
+        {
+
+            using (WebApiEntities1 entities = new WebApiEntities1())
+            {
+
+                return entities.Employes.ToList();
+            }
+        }
+        public Employe Get(int id)
+        {
+            using(WebApiEntities1 entities1= new WebApiEntities1())
+            {
+                var model=entities1.Employes.FirstOrDefault(e => e.ID == id);
+                return model;
+            }
+        }
+
+    }
+}
+    
